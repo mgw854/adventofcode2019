547504e5c2cc1ecef1e6933dee0ae883ae992315
Matthew White <mwhite@plex.com>
2019-12-01 18:21:07 -0500

    Scaffold for day 2

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use std::error::Error;
 mod inputhandling;
 mod day1;
 
-fn main() -> Result<(), Box<Error>> {
-    let input : Vec<u32> = inputhandling::parse_input(1, |s| s.parse::<u32>().map_err(|e| e.into()))?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let input : Vec<u32> = inputhandling::parse_input(2, |s| s.parse::<u32>().map_err(|e| e.into()))?;
     
     let sum : u32 = input.iter().map(|mass| day1::calculate_needed_fuel(*mass)).sum();
 
