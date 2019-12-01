4b3d1fc71ed43313982801406d3eef9137b85c4a
Matthew White <mgw854@msn.com>
2019-12-01 14:04:31 -0500

    Remove hard-coded path for input files based on current machine

--- src/inputhandling.rs
+++ src/inputhandling.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
+use std::path::Path;
 
 pub fn parse_input<T>(day: u8, parser: fn(&str) -> Result<T, Box<Error>>) -> Result<Vec<T>, Box<Error>>
 {
-    let mut file = File::open(format!("F:\\Development Ideas\\adventofcode2019\\src\\day{}\\input.txt", day))?;
+    let pathstr = format!(".\\src\\day{}\\input.txt", day);
+    let path = Path::new(&pathstr);
+    let mut file = File::open(path)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     contents.lines().map(|s| s.trim()).map(|s| parser(s)).collect()
