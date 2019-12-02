efcc0e000c6ab54def823c9ef18087fd1dabd945
Matthew White <mwhite@plex.com>
2019-12-02 11:53:50 -0500

    Solved day 2, part 2

--- src/main.rs
+++ src/main.rs
@@ -4,33 +4,72 @@ mod inputhandling;
 mod day1;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input : Vec<usize> = inputhandling::parse_csv_input(2, |s| s.parse::<usize>().map_err(|e| e.into()))?;
+    let mut nounverb = 0;
+
+    let mut noun = 0;
+    let mut verb = 0;
+
+    while noun < 100
+    {
+        verb = 0;
+        while verb < 100
+        {
+
+            let mut input : Vec<usize> = inputhandling::parse_csv_input(2, |s| s.parse::<usize>().map_err(|e| e.into()))?;
+        
+            input[1] = noun;
+            input[2] = verb;
+        
+            let mut i = 0;
+        
+            while i < input.len() {
+                match input[i] {
+                    1 => {
+                        if i + 4 >= input.len() {
+                            break;
+                        }
+
+                        let store = input[i + 3];
+
+                        if (store >= input.len() || input[i + 1] >= input.len() || input[i+2] >= input.len()){
+                            break;
+                        }
+                        let result = input[input[i + 1]] + input[input[i + 2]];
+                        input[store] = result;
+                    },
+                    2 => {
+                        if i + 4 >= input.len() {
+                            break;
+                        }
+
+                        let store = input[i + 3];
+                        if (store >= input.len() || input[i + 1] >= input.len() || input[i+2] >= input.len()){
+                            break;
+                        }
+                        let result = input[input[i + 1]] * input[input[i + 2]];
+                        input[store] = result;
+                    },
+                    99 => break,
+                    _ => continue
+                }
+        
+                i += 4;
+            }
     
-    input[1] = 12;
-    input[2] = 2;
+            nounverb = input[0];
+        
+            if nounverb == 19690720 {
+                println!("100*noun+verb = {}", 100 * noun + verb);
+                return Ok(());
+            }
 
-    let mut i = 0;
-
-    while i < input.len() {
-        match input[i] {
-            1 => {
-                let store = input[i + 3];
-                let result = input[input[i + 1]] + input[input[i + 2]];
-                input[store] = result;
-            },
-            2 => {
-                let store = input[i + 3];
-                let result = input[input[i + 1]] * input[input[i + 2]];
-                input[store] = result;
-            },
-            99 => break,
-            _ => continue
+            verb += 1;
         }
 
-        i += 4;
+        noun += 1;
     }
+    
 
-    println!("The value at position 0 is {}", input[0]);
     
     Ok(())
 }
