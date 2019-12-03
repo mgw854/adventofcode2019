f70b1037815cc72c3403ca462b682081658a77f0
Matthew White <mwhite@plex.com>
2019-12-03 15:05:02 -0500

    Day 3, Part 1 solved

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,12 @@ mod day3;
 fn main() -> Result<(), Box<dyn Error>> {
   let mut input : Vec<Vec<day3::WiringInstruction>> = inputhandling::parse_input_per_line(3, |s| day3::WiringInstruction::parse(s).map_err(|e| e.into()))?;
 
+  let one = day3::generate_points(&input[0]);
+  let two = day3::generate_points(&input[1]);
+
+  let distance = day3::generate_manhattan_distance(&one, &two);
+
+  println!("The Manhattan distance is {}", distance);
+
   Ok(())
 }
