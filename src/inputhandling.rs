b069855b7f6df35a8157f2d157fdaea38d3b9594
Matthew White <mwhite@plex.com>
2019-12-03 14:32:01 -0500

    Parse the input for day 3

--- src/inputhandling.rs
+++ src/inputhandling.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::error::Error;
 use std::path::Path;
 
-pub fn parse_input<T>(day: u8, parser: fn(&str) -> Result<T, Box<Error>>) -> Result<Vec<T>, Box<Error>>
+pub fn parse_input<T>(day: u8, parser: fn(&str) -> Result<T, Box<dyn Error>>) -> Result<Vec<T>, Box<dyn Error>>
 {
     let pathstr = format!(".\\src\\day{}\\input.txt", day);
     let path = Path::new(&pathstr);
@@ -13,7 +13,20 @@ pub fn parse_input<T>(day: u8, parser: fn(&str) -> Result<T, Box<Error>>) -> Res
     contents.lines().map(|s| s.trim()).map(|s| parser(s)).collect()
 }
 
-pub fn parse_csv_input<T>(day: u8, parser: fn(&str) -> Result<T, Box<Error>>) -> Result<Vec<T>, Box<Error>>
+pub fn parse_input_per_line<T>(day: u8, parser: fn(&str) -> Result<T, Box<dyn Error>>) -> Result<Vec<Vec<T>>, Box<dyn Error>>
+{
+    let pathstr = format!(".\\src\\day{}\\input.txt", day);
+    let path = Path::new(&pathstr);
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    contents
+      .lines()
+      .map(|s| s.trim().split(",").map(|x| parser(x)).collect())
+      .collect()
+}
+
+pub fn parse_csv_input<T>(day: u8, parser: fn(&str) -> Result<T, Box<dyn Error>>) -> Result<Vec<T>, Box<dyn Error>>
 {
     let pathstr = format!(".\\src\\day{}\\input.txt", day);
     let path = Path::new(&pathstr);
--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,7 @@ mod intcode;
 mod day3;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut nounverb = 0;
+  let mut input : Vec<Vec<day3::WiringInstruction>> = inputhandling::parse_input_per_line(3, |s| day3::WiringInstruction::parse(s).map_err(|e| e.into()))?;
 
-    let mut noun = 0;
-    let mut verb = 0;
-
-    while noun < 100
-    {
-        verb = 0;
-        while verb < 100
-        {
-
-            let mut input : Vec<usize> = inputhandling::parse_csv_input(2, |s| s.parse::<usize>().map_err(|e| e.into()))?;
-        
-            input[1] = noun;
-            input[2] = verb;
-        
-            let cpu = intcode::create(input);
-    
-            nounverb = cpu.process();
-        
-            if nounverb == 19690720 {
-                println!("100*noun+verb = {}", 100 * noun + verb);
-                return Ok(());
-            }
-
-            verb += 1;
-        }
-
-        noun += 1;
-    }
-    
-
-    
-    Ok(())
+  Ok(())
 }
