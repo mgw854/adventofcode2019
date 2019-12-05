259a421bec9f4e4b07f1ee2b0f5bf71a4e5b0e20
Matthew White <mwhite@plex.com>
2019-12-04 21:37:18 -0500

    Create an iterator for removing runs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 mod inputhandling;
 mod intcode;
 mod day4;
+mod fancyiters;
 
 fn main() -> Result<(), Box<dyn Error>> {
   //let mut input : Vec<Vec<day3::WiringInstruction>> = inputhandling::parse_input_per_line(3, |s| day3::WiringInstruction::parse(s).map_err(|e| e.into()))?;
