5af674707a3c5bf3d283d7b77bd71f97198b6efa
Matthew White <mwhite@plex.com>
2019-12-08 01:06:06 -0500

    Day 8, Part 2 done!

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,20 @@ mod inputhandling;
 fn main() -> Result<(), Box<dyn Error>> {
   let inputs = inputhandling::get_input_chars(8)?;
 
-  let mut layers : HashMap<u32, HashMap<char, u32>> = HashMap::new();
-  let mut layer = 0;
+  let mut layers : HashMap<u32, HashMap<u8, u32>> = HashMap::new();
+  let mut layer = 1;
 
   for chunk in inputs.chunks(25*6) {
-    let mut map : HashMap<char, u32> = HashMap::new();
+    let mut map : HashMap<u8, u32> = HashMap::new();
     for c in chunk {
-      let count = map.get(c).cloned().unwrap_or(0);
-      map.insert(*c, count + 1);
+      let cv = match c {
+        '0' => 0,
+        '1' => 1,
+        '2' => 2,
+        _ => panic!("Not supposed to get here")
+      };
+      let count = map.get(&cv).cloned().unwrap_or(0);
+      map.insert(cv, count + 1);
     }
 
     layers.insert(layer, map);
@@ -22,18 +28,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     layer += 1;
   }
 
-  let mut least_zeroes = 2000000000;
+  use bmp::{ Image, Pixel };
 
-  for (k, v) in layers.iter() {
-    if v.get(&'0').cloned().unwrap_or(2000000) < least_zeroes {
-      layer = *k;
-      least_zeroes = v.get(&'0').cloned().unwrap()
+  let mut image = Image::new(25, 6);
+
+  for i in 0..25 as u32 {
+    for j in 0..6 as u32 {
+      for chunk in inputs.chunks(25*6) {
+       match chunk.iter().skip((j as usize * 25) + i as usize).nth(0).unwrap() {
+         '0' => { image.set_pixel(i, j, Pixel::new(0,0,0)); break; },
+         '1' => { image.set_pixel(i, j, Pixel::new(255,255,255)); break; }
+         '2' => continue,
+         _ => panic!("Can't get here")
+        }
+      }
     }
   }
-
-  let min_layer = layers.get(&layer).unwrap();
-
-  println!("The numbers of ones times two is {}", min_layer.get(&'1').cloned().unwrap() * min_layer.get(&'2').cloned().unwrap());
+  
+  let _ = image.save(".\\src\\day8\\img.bmp");
 
   Ok(())
 }
