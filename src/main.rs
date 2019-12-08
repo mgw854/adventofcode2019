932c48c48711e43f37f48ea33541632964ea51a2
Matthew White <mwhite@plex.com>
2019-12-08 00:41:07 -0500

    Day 8, Part 1 done

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,39 @@
 use std::error::Error;
+use std::collections::HashMap;
 
-mod day7;
 mod fancyiters;
 mod inputhandling;
-mod intcode;
 
 fn main() -> Result<(), Box<dyn Error>> {
-  let vonNeumann : Vec<i32> = inputhandling::parse_csv_input(7, |s| s.parse::<i32>().map_err(|e| e.into()))?;
+  let inputs = inputhandling::get_input_chars(8)?;
 
-  let mut max = 0;
+  let mut layers : HashMap<u32, HashMap<char, u32>> = HashMap::new();
+  let mut layer = 0;
 
-  for sequence in day7::phase_setting_generator() {
-    let cpu0 = intcode::Intcode::create(vonNeumann.clone());
-    let cpu1 = intcode::Intcode::create(vonNeumann.clone());
-    let cpu2 = intcode::Intcode::create(vonNeumann.clone());
-    let cpu3 = intcode::Intcode::create(vonNeumann.clone());
-    let cpu4 = intcode::Intcode::create(vonNeumann.clone());
-
-    let mut amp0 = day7::Amplifier::create(sequence[0], 0, cpu0);
-    let mut amp1 = day7::Amplifier::create_no_value(sequence[1], cpu1);
-    let mut amp2 = day7::Amplifier::create_no_value(sequence[2], cpu2);
-    let mut amp3 = day7::Amplifier::create_no_value(sequence[3], cpu3);
-    let mut amp4 = day7::Amplifier::create_no_value(sequence[4], cpu4);
-
-    let mut halted = false;
-
-    while !halted {
-      amp1.input_signal = match amp0.run() {
-        day7::AmplifierState::Halt { value: x } => {halted = true; x}
-        day7::AmplifierState::Hot { value: x } => x 
-      };
-
-      amp2.input_signal = match amp1.run() {
-        day7::AmplifierState::Halt { value: x } => {halted = true; x}
-        day7::AmplifierState::Hot { value: x } => x 
-      };
-
-      amp3.input_signal = match amp2.run() {
-        day7::AmplifierState::Halt { value: x } => {halted = true; x}
-        day7::AmplifierState::Hot { value: x } => x 
-      };
-
-      amp4.input_signal = match amp3.run() {
-        day7::AmplifierState::Halt { value: x } => {halted = true; x}
-        day7::AmplifierState::Hot { value: x } => x 
-      };
-
-      amp0.input_signal = match amp4.run() {
-        day7::AmplifierState::Halt { value: x } => { halted = true; x },
-        day7::AmplifierState::Hot { value: x } => x 
-      };
+  for chunk in inputs.chunks(25*6) {
+    let mut map : HashMap<char, u32> = HashMap::new();
+    for c in chunk {
+      let count = map.get(c).cloned().unwrap_or(0);
+      map.insert(*c, count + 1);
     }
 
-    if amp0.input_signal > max {
-      max = amp0.input_signal;
+    layers.insert(layer, map);
+
+    layer += 1;
+  }
+
+  let mut least_zeroes = 2000000000;
+
+  for (k, v) in layers.iter() {
+    if v.get(&'0').cloned().unwrap_or(2000000) < least_zeroes {
+      layer = *k;
+      least_zeroes = v.get(&'0').cloned().unwrap()
     }
   }
 
-  println!("The maximum output is {}", max);
+  let min_layer = layers.get(&layer).unwrap();
+
+  println!("The numbers of ones times two is {}", min_layer.get(&'1').cloned().unwrap() * min_layer.get(&'2').cloned().unwrap());
 
   Ok(())
 }
